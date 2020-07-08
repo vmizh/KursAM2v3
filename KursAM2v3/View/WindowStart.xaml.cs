e4c349cc9bec754bd161902056779e56f0786d6b
vmizh <vmizhg@gmail.com>
2020-07-08 12:38:28 +0300

    08072020

--- KursAM2v3/View/WindowStart.xaml.cs
+++ KursAM2v3/View/WindowStart.xaml.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace KursAM2.View
 {
     /// <summary>
-    /// Interaction logic for WindowStart.xaml
+    ///     Interaction logic for WindowStart.xaml
     /// </summary>
-    public partial class WindowStart : Window
+    public partial class WindowStart
     {
         public WindowStart()
         {
             InitializeComponent();
             Loaded += WindowStart_Loaded;
         }
-       
+
         private void WindowStart_Loaded(object sender, RoutedEventArgs e)
         {
-            var main = new MainWindow { Owner = Application.Current.MainWindow };
+            var main = new MainWindow {Owner = Application.Current.MainWindow};
             main.Show();
         }
     }
