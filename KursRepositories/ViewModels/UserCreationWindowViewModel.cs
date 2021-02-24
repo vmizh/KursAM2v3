2a90de552b92e0e445f9320bd5de0ce2fea89909
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-02-24 14:19:10 +0300

    Обновил форму создания пользователя и viewmodel

--- KursRepositories/ViewModels/UserCreationWindowViewModel.cs
+++ KursRepositories/ViewModels/UserCreationWindowViewModel.cs
@@ -4,33 +4,66 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Core.ViewModel.Base;
+using Data;
 
 namespace KursRepositories.ViewModels
 {
     class UserCreationWindowViewModel : RSViewModelBase
     {
-        public UserCreationWindowViewModel ()
+        public UserCreationWindowViewModel()
         {
-           
-        }
+        /*public string Error
+        {
+            get
+            {
+                string error =
+                ["FirstName"] +
+                        this["LastName"] +
+                    this["Title"] +
+                    this["MobilePhone"] +
+                    this["Email"];
+                if (!string.IsNullOrEmpty(error))
+                    return "Please check inputted data.";
+                return null;
+            }
+        }*/
+
+    }
 
         #region Fields
 
         private string myFirstName;
         private string myMiddleName;
         private string myLastName;
-        private string myFullName => $"{LastName} {FirstName} {MiddleName}";
-        private bool myAdmin;
-        private bool myTester;
+        private string myFullName;
+        private string myLoginName;
+        private bool myAdmin = false;
+        private bool myTester = false;
         private bool myDeleted = false;
-        private byte myAvatar;
+        private byte[] myAvatar;
+        private string myThemeName = "MetropolisLight";
+        private new string myNote;
+        private ObservableCollection<UsersViewModel> myNewUser = new ObservableCollection<UsersViewModel>();
+
 
         #endregion
 
         #region Properties
 
-        private ObservableCollection<UsersViewModel> NewUser { get; set; } = new ObservableCollection<UsersViewModel>();
+        public ObservableCollection<UsersViewModel> NewUser
+        {
+            get => myNewUser;
+            set
+            {
+                if (myNewUser == value)
+                    return;
+                myNewUser = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public string FirstName
         {
@@ -40,6 +73,7 @@ namespace KursRepositories.ViewModels
                 if (myFirstName == null)
                     return;
                 myFirstName = value;
+                RaiseFullNamePropertyChanged();
                 RaisePropertyChanged();
             }
         }
@@ -49,9 +83,10 @@ namespace KursRepositories.ViewModels
             get => myMiddleName;
             set
             {
-                if(myMiddleName == value)
+                if (myMiddleName == value)
                     return;
                 myMiddleName = value;
+                RaiseFullNamePropertyChanged();
                 RaisePropertyChanged();
             }
         }
@@ -61,14 +96,25 @@ namespace KursRepositories.ViewModels
             get => myLastName;
             set
             {
-                if(myLastName == value)
+                if (myLastName == value)
                     return;
                 myLastName = value;
+                RaiseFullNamePropertyChanged();
                 RaisePropertyChanged();
             }
         }
 
-        public string FullName => myFullName ;
+        public string FullName
+        {
+            get => $"{LastName}  {FirstName} {MiddleName}";
+            set
+            {
+                if (myFullName == value)
+                    return;
+                myFullName = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public bool Admin
         {
@@ -78,6 +124,18 @@ namespace KursRepositories.ViewModels
                 if (myAdmin == value)
                     return;
                 myAdmin = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string LoginName
+        {
+            get => myLoginName;
+            set
+            {
+                if (myLoginName == value)
+                    return;
+                myLoginName = value;
                 RaisePropertyChanged();
             }
         }
@@ -96,22 +154,80 @@ namespace KursRepositories.ViewModels
 
         public bool Deleted => myDeleted;
 
-        public byte Avatar
+        public byte[] Avatar
         {
             get => myAvatar;
             set
             {
-                if(myAvatar == value)
+                if (myAvatar == value)
                     return;
                 myAvatar = value;
                 RaisePropertyChanged();
             }
         }
-        
+
+        public string ThemeName
+        {
+            get => myThemeName;
+            set
+            {
+                if (myThemeName == value)
+                    return;
+                myThemeName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override string Note
+        {
+            get => myNote;
+            set
+            {
+                if (myNote == value)
+                    return;
+                myNote = value;
+                RaisePropertyChanged();
+            }
+        }
+
         #endregion
 
+        #region Command
+
+        public ICommand CreateNewUserCommand =>
+            new Command(createNewUserCommand, _ => LoginName != null && FullName != null);
+
+        private void createNewUserCommand(object obj)
+        {
+            NewUser.Add(new UsersViewModel(new Users
+            {
+                Id = new Guid(),
+                Name = LoginName,
+                FullName = FullName,
+                Note = Note,
+                ThemeName = ThemeName,
+                IsAdmin = Admin,
+                IsTester = Tester,
+                IsDeleted = Deleted,
+                Avatar = Avatar
+
+            }));
 
 
+        }
+
+        #endregion
+
+        #region Methods
+
+        void RaiseFullNamePropertyChanged()
+        {
+            RaisePropertyChanged(nameof(FullName));
+
+        }
+
+        
+
+        #endregion
     }
-    
 }
