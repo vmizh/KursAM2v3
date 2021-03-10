8986d1b96075db079abc60bbae574fdce28ae754
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-03-10 15:32:10 +0300

    Убрал лишний столбец в ролях

--- KursRepositories/View/UserCreationWindow.xaml.cs
+++ KursRepositories/View/UserCreationWindow.xaml.cs
@@ -1,21 +1,4 @@
-﻿using DevExpress.Xpf.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using DevExpress.Xpf.Editors;
-using KursRepositories.ViewModels;
-
-
-namespace KursRepositories.View
+﻿namespace KursRepositories.View
 {
     /// <summary>
     /// Interaction logic for UserCreationWindow.xaml
@@ -29,6 +12,7 @@ namespace KursRepositories.View
 
         }
 
-      
+
+        
     }
 }
