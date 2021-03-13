e99e23f2aec96b81143f62ff0a168dc790863657
Вадим Мижинский <vmizhg@gmail.com>
2021-03-13 19:04:39 +0300

    Поправил xaml и добавил сохранение разметки

--- KursAM2v3/View/Repozit/UsersManagerView.xaml.cs
+++ KursAM2v3/View/Repozit/UsersManagerView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
+using DevExpress.Xpf.Editors.Settings;
 using DevExpress.Xpf.Grid;
+using LayoutManager;
 
 namespace KursAM2.View.Repozit
 {
@@ -11,21 +13,37 @@ namespace KursAM2.View.Repozit
         public UsersManagerView()
         {
             InitializeComponent();
+            LayoutManager = new LayoutManager.LayoutManager(GetType().Name, this, LayoutControlMain);
+            Loaded += UsersManagerView_Loaded;
+            Closing += UsersManagerView_Closing;
         }
+
+        private void UsersManagerView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            LayoutManager.Save();
+        }
+
+        private void UsersManagerView_Loaded(object sender, RoutedEventArgs e)
+        {
+            LayoutManager.Load();
+        }
+
+        public LayoutManager.LayoutManager LayoutManager { get; set; }
+        public string LayoutManagerName { get; set; }
 
         private void RolesGridControl_OnAutoGeneratingColumn(object sender, AutoGeneratingColumnEventArgs e)
         {
             foreach (GridColumn column in RolesGridControl.Columns)
             {
+                e.Column.Name = e.Column.FieldName;
                 if (column.FieldName == "IsSelectedItem")
                     column.Visible = false;
-
             }
         }
 
-
         private void UsersGridControl_OnAutoGeneratingColumn(object sender, AutoGeneratingColumnEventArgs e)
         {
+            e.Column.Name = e.Column.FieldName;
             e.Column.ReadOnly = true;
         }
 
@@ -33,6 +51,13 @@ namespace KursAM2.View.Repozit
         {
             e.Column.Name = e.Column.FieldName;
             if(e.Column.FieldName != "IsSelectedItem")
+                e.Column.ReadOnly = true;
+        }
+
+        private void GridControlPermissionItems_OnAutoGeneratingColumn(object sender, AutoGeneratingColumnEventArgs e)
+        {
+            e.Column.Name = e.Column.FieldName;
+            if (e.Column.Name != "IsSelectedItem")
                 e.Column.ReadOnly = true;
         }
     }
