385646058cbe49151ad13db7f1a9987d537be64b
Vitaliy Lukin <mrlvs2015@gmail.com>
2021-03-29 18:28:04 +0300

    Форма отчета Расходный складской ордер

---
+++ KursAM2v3/Reports/WarehouseOrderOut.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace KursAM2.Reports
+{
+    public partial class WarehouseOrderOut : DevExpress.XtraReports.UI.XtraReport
+    {
+        public WarehouseOrderOut()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
