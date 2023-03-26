90c773436fc583ac4c4168bf0031034521e38d54
Вадим Мижинский <vmizhg@gmail.com>
2023-03-27 00:49:30 +0300

    Русификация табличного меню

--- Helper/CustomDXGridLocalizer.cs
+++ Helper/CustomDXGridLocalizer.cs
@@ -16,6 +16,22 @@ namespace Helper
             //AddString(GridControlStringId.C, "Customize Totals");
             AddString(GridControlStringId.ColumnChooserCaption, "Выбор колонок");
             AddString(GridControlStringId.MenuColumnShowColumnChooser, "Выбор колонок");
+            AddString(GridControlStringId.MenuColumnSortAscending,"По возрастанию");
+            AddString(GridControlStringId.MenuColumnSortDescending,"По убыванию");
+            AddString(GridControlStringId.MenuColumnBestFit, "Автоширина");
+            AddString(GridControlStringId.MenuColumnBestFitColumns, "Автоширина для всех");
+            AddString(GridControlStringId.MenuColumnClearFilter, "Очистить фильтр");
+            AddString(GridControlStringId.MenuColumnClearSorting, "Убрать сортировку");
+            AddString(GridControlStringId.MenuColumnFilterEditor, "Изменить фильтр");
+            AddString(GridControlStringId.MenuColumnShowSearchPanel, "Показать панель поиска");
+            AddString(GridControlStringId.MenuColumnGroup, "Сгруппировать по колонке");
+            AddString(GridControlStringId.MenuColumnHideGroupPanel, "Скрыть панель группировки");
+            AddString(GridControlStringId.MenuColumnShowGroupPanel, "Показать панель группировки");
+            AddString(GridControlStringId.GridGroupPanelText, "Панель группировки");
+            AddString(GridControlStringId.MenuGroupPanelFullExpand, "Раскрыть все");
+            AddString(GridControlStringId.MenuGroupPanelFullCollapse, "Свернуть все");
+            AddString(GridControlStringId.MenuGroupPanelClearGrouping, "Очистить группировку");
+
 
         }
     }
