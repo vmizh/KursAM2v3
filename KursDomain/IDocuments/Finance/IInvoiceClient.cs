1ac98843c30cab2591153cd3104bb5ec781146d8
norman187 <norman187@yandex.ru>
2023-11-01 16:26:47 +0300

    Выствлена правильная разметка при вызове интерфейса сф-ры при вызове из банковской проводки

--- KursDomain/IDocuments/Finance/IInvoiceClient.cs
+++ KursDomain/IDocuments/Finance/IInvoiceClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using DevExpress.Mvvm.DataAnnotations;
 using KursDomain.Documents.Invoices;
 using KursDomain.References;
 
@@ -12,68 +13,68 @@ public interface IInvoiceClient
 
     [Display(AutoGenerateField = false)] Guid Id { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Поставщик")]
+    [Display(AutoGenerateField = true, Name = "Поставщик", Order = 9)]
     Kontragent Receiver { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Центр ответственности")]
+    [Display(AutoGenerateField = true, Name = "Центр ответственности", Order = 12)]
     CentrResponsibility CO { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Тип продукции")]
+    [Display(AutoGenerateField = true, Name = "Тип продукции", Order = 13)]
     NomenklProductType VzaimoraschetType { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Форма расчетов")]
+    [Display(AutoGenerateField = true, Name = "Форма расчетов", Order = 15)]
     PayForm FormRaschet { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Условия оплаты")]
+    [Display(AutoGenerateField = true, Name = "Условия оплаты",Order = 14) ]
     PayCondition PayCondition { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Дата")]
+    [Display(AutoGenerateField = true, Name = "Дата", Order = 1)]
     DateTime DocDate { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "№")]
+    [Display(AutoGenerateField = true, Name = "№", Order = 2)]
     int InnerNumber { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Внешний №")]
+    [Display(AutoGenerateField = true, Name = "Внешний №", Order = 3)]
     string OuterNumber { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Клиент")]
+    [Display(AutoGenerateField = true, Name = "Клиент", Order = 4)]
     Kontragent Client { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Валюта")]
+    [Display(AutoGenerateField = true, Name = "Валюта", Order = 6)]
     References.Currency Currency { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Отгружено")]
+    [Display(AutoGenerateField = true, Name = "Отгружено", Order = 8)]
     [DisplayFormat(DataFormatString = "n2", NullDisplayText = "0")]
     decimal SummaOtgruz { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Сумма дилера")]
+    [Display(AutoGenerateField = true, Name = "Сумма дилера", Order = 17)]
     [DisplayFormat(DataFormatString = "n2", NullDisplayText = "0")]
     decimal DilerSumma { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Примечание")]
+    [Display(AutoGenerateField = true, Name = "Примечание", Order = 10)]
     string Note { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Дилер")]
+    [Display(AutoGenerateField = true, Name = "Дилер", Order = 16)]
     Kontragent Diler { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Акцептован")]
+    [Display(AutoGenerateField = true, Name = "Акцептован", Order = 18)]
     bool IsAccepted { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Сумма")]
+    [Display(AutoGenerateField = true, Name = "Сумма", Order = 5)]
     [DisplayFormat(DataFormatString = "n2", NullDisplayText = "0")]
     decimal Summa { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Создатель")]
+    [Display(AutoGenerateField = true, Name = "Создатель", Order = 11)]
     string CREATOR { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "НДС в цене")]
+    [Display(AutoGenerateField = true, Name = "НДС в цене", Order = 19)]
     bool IsNDSIncludeInPrice { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Оплачено")]
+    [Display(AutoGenerateField = true, Name = "Оплачено", Order = 7)]
     [DisplayFormat(DataFormatString = "n2", NullDisplayText = "0")]
     decimal PaySumma { set; get; }
 
-    [Display(AutoGenerateField = true, Name = "Ответственный")]
+    [Display(AutoGenerateField = true, Name = "Ответственный", Order = 20)]
     References.Employee PersonaResponsible { set; get; }
 
     [Display(AutoGenerateField = false)] ObservableCollection<IInvoiceClientRow> Rows { set; get; }
