af3cd8e3aa104c82c7d9f47a81063cb6ff1c55be
Вадим Мижинский <vmizhg@gmail.com>
2024-07-24 13:38:11 +0300

    StartLogin

--- KursAM2v3/View/StartLogin.xaml.cs
+++ KursAM2v3/View/StartLogin.xaml.cs
@@ -79,14 +79,13 @@ namespace KursAM2.View
                 dtx.SelectedDataSource = e.NewValue as DataSource;
         }
 
-        private async void pwdText_KeyDown(object sender, KeyEventArgs e)
+        private void pwdText_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
                 
                 ((StartLoginViewModel)DataContext).IsConnectNotExecute = false;
-                ButtonOK.Background =
-                    (SolidColorBrush)new BrushConverter().ConvertFrom("#9ae4ff");
+                ButtonOK.Tag = "Active";
                 if (mySubscriber != null && mySubscriber.IsConnected())
                 {
                     var message = new RedisMessage
