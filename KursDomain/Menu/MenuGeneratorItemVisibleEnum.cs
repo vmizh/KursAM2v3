39ed52c44a8b3b15a76a46949ac689183269ccec
Вадим Мижинский <vmizhg@gmail.com>
2024-09-10 11:01:19 +0300

    Настройка видимости основного меню

---
+++ KursDomain/Menu/MenuGeneratorItemVisibleEnum.cs
@@ -0,0 +1,21 @@
+﻿namespace KursDomain.Menu;
+
+public enum MenuGeneratorItemVisibleEnum
+{
+    None = 0,
+    Done = 1,
+    Exit = 2, 
+    DocumentOpen = 14,
+    DocumentAdd = 3,
+    DocumentNewEmpty = 4,
+    DocumentNewRequisite = 5,
+    DocumentNewCopy = 6, 
+    DocDelete = 10,
+    Refresh = 7,
+    Save = 8,
+    Printer = 9,
+    Options = 11,
+    ResetLayout = 12,
+    AddSearchlist = 13
+   
+}
