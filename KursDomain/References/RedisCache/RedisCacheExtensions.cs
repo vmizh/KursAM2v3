c45117649f2f08bf6ebbbd2124cdbd1c59140c69
vmizh <vmizhg@gmail.com>
2025-11-04 10:47:46 +0300

    Добавка к предыдущемо коммиту

--- KursDomain/References/RedisCache/RedisCacheExtensions.cs
+++ KursDomain/References/RedisCache/RedisCacheExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace KursDomain.References.RedisCache;
@@ -10,29 +9,31 @@ public static class RedisCacheExtensions
     {
         var dcOlds = caches.Where(_ => _.DocCode == cacheKey.DocCode).ToList();
         var idOlds = caches.Where(_ => _.Id == cacheKey.Id).ToList();
-        foreach (var item in dcOlds.Where(item => dcOlds.Count > 0))
-        {
-            caches.Remove(item);
-        }
-        foreach (var item in idOlds.Where(item => idOlds.Count > 0))
-        {
-            caches.Remove(item);
-        }
+        foreach (var item in dcOlds.Where(item => dcOlds.Count > 0)) caches.Remove(item);
+        foreach (var item in idOlds.Where(item => idOlds.Count > 0)) caches.Remove(item);
         caches.Add(cacheKey);
     }
 
     public static void AddCacheKey(this HashSet<CachKey> caches, CachKey cacheKey)
     {
-        var dcOlds = caches.Where(_ => _.DocCode == cacheKey.DocCode).ToList();
-        var idOlds = caches.Where(_ => _.Id == cacheKey.Id).ToList();
-        foreach (var item in dcOlds.Where(item => dcOlds.Count > 0))
-        {
-            caches.Remove(item);
-        }
-        foreach (var item in idOlds.Where(item => idOlds.Count > 0))
-        {
-            caches.Remove(item);
-        }
         caches.Add(cacheKey);
+
+        var dcOlds = caches.Where(_ => _.DocCode!= null && _.DocCode == cacheKey.DocCode).ToList();
+        var idOlds = caches.Where(_ => _.Id != null &&_.Id == cacheKey.Id).ToList();
+        if (dcOlds.Count > 1)
+        {
+            var dt = dcOlds.Max(_ => _.LastUpdate);
+            foreach (var item in dcOlds.Where(item => dcOlds.Count > 0))
+                if (item.LastUpdate != dt)
+                    caches.Remove(item);
+        }
+
+        if (idOlds.Count > 1)
+        {
+            var dt = idOlds.Max(_ => _.LastUpdate);
+            foreach (var item in idOlds.Where(item => idOlds.Count > 0))
+                if (item.LastUpdate != dt)
+                    caches.Remove(item);
+        }
     }
 }
