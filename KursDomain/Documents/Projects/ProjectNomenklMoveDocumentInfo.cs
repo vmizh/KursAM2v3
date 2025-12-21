7e0157ea9803da8420aff49cd1c8033575f06229
vmizh <vmizhg@gmail.com>
2025-12-21 08:58:05 +0300

    Добавка к предыдущему коммиту

--- KursDomain/Documents/Projects/ProjectNomenklMoveDocumentInfo.cs
+++ KursDomain/Documents/Projects/ProjectNomenklMoveDocumentInfo.cs
@@ -19,62 +19,98 @@ public class ProjectNomenklMoveDocumentInfo
 
     [Display(AutoGenerateField = false)]
     public DocumentType DocumentType { set; get; }
-    [Display(AutoGenerateField = true, Name = "Тип док-та", Order = 1)]
+    [Display(AutoGenerateField = true, Name = "Тип док-та", GroupName = "Основные данные", Order = 1)]
     public string DocType => DocumentType.GetDisplayAttributesFrom(DocumentType.GetType()).Name;
 
-    [Display(AutoGenerateField = true,  Name = "Внут.№", Order = 2)]
+    [Display(AutoGenerateField = true,  Name = "Внут.№", GroupName = "Основные данные", Order = 2)]
     public int? InnerNumber { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Внеш.№", Order = 3)]
+    [Display(AutoGenerateField = true,  Name = "Внеш.№", GroupName = "Основные данные", Order = 3)]
     public string ExtNumber { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Дата", Order = 4)]
+    [Display(AutoGenerateField = true, Name = "Дата", GroupName = "Основные данные", Order = 4)]
     public DateTime DocDate { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Контрагент", Order = 5)]
+    [Display(AutoGenerateField = true, Name = "Контрагент", GroupName = "Основные данные", Order = 5)]
     public Kontragent Kontragent { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Дилер", Order = 6)]
+    [Display(AutoGenerateField = true, Name = "Дилер", GroupName = "Основные данные", Order = 6)]
     public Kontragent Diler { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Склад", Order = 7)]
+    [Display(AutoGenerateField = true, Name = "Склад", GroupName = "Основные данные", Order = 7)]
     public References.Warehouse Warehouse { set; get; }
-    [Display(AutoGenerateField = true,  Name = "Примечание", Order = 8)]
+    [Display(AutoGenerateField = true, Name = "Примечание", GroupName = "Основные данные", Order = 8)]
     public string Note { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Создатель", Order = 9)]
+    [Display(AutoGenerateField = true, Name = "Создатель", GroupName = "Основные данные", Order = 9)]
     public string Creator { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Сумма (поставщик)", Order = 10)]
+    [Display(AutoGenerateField = true, Name = "Сумма (поставщик)", GroupName = "Документ", Order = 10)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ProviderSumma { set; get; }
-    [Display(AutoGenerateField = true,  Name = "Сумма (клиент)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Сумма (клиент)", GroupName = "Документ", Order = 11)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ClientSumma { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Отгружено (поставщик)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Отгружено (поставщик)", GroupName = "Документ", Order = 12)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ProviderShipped { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Отгружено (клиент)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Отгружено (клиент)", GroupName = "Документ", Order = 13)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ClientShipped { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Кол-во (поставщик)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Кол-во (поставщик)", GroupName = "Документ", Order = 14)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ProviderQuantity { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Кол-во (клиент)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Кол-во (клиент)", GroupName = "Документ", Order = 15)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ClientQuantity { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Кол-во отгр. (поставщик)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Кол-во отгр. (поставщик)", GroupName = "Документ", Order = 16)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ProviderShippedQuantity { set; get; }
 
-    [Display(AutoGenerateField = true,  Name = "Кол-во отгр. (клиент)", Order = 10)]
+    [Display(AutoGenerateField = true,  Name = "Кол-во отгр. (клиент)", GroupName = "Документ", Order = 17)]
     [DisplayFormat(DataFormatString = "n2")]
     public decimal ClientShippedQuantity { set; get; }
 
+
     [Display(AutoGenerateField = true)] public bool IsInclude { set; get; } = false;
+
+    #region Ручная установка
+
+    [Display(AutoGenerateField = true, Name = "Сумма (поставщик)", GroupName = "Учтено в проекте", Order = 18)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualProviderSumma { set; get; }
+    [Display(AutoGenerateField = true, Name = "Сумма (клиент)", GroupName = "Учтено в проекте", Order = 19)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualClientSumma { set; get; }
+
+    [Display(AutoGenerateField = true, Name = "Отгружено (поставщик)", GroupName = "Учтено в проекте", Order = 20)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualProviderShipped { set; get; }
+
+    [Display(AutoGenerateField = true, Name = "Отгружено (клиент)", GroupName = "Учтено в проекте", Order = 21)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualClientShipped { set; get; }
+
+    [Display(AutoGenerateField = true, Name = "Кол-во (поставщик)", GroupName = "Учтено в проекте", Order = 22)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualProviderQuantity { set; get; }
+
+    [Display(AutoGenerateField = true, Name = "Кол-во (клиент)", GroupName = "Учтено в проекте", Order = 23)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualClientQuantity { set; get; }
+
+    [Display(AutoGenerateField = true, Name = "Кол-во отгр. (поставщик)", GroupName = "Учтено в проекте", Order = 24)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualProviderShippedQuantity { set; get; }
+
+    [Display(AutoGenerateField = true, Name = "Кол-во отгр. (клиент)", GroupName = "Учтено в проекте", Order = 25)]
+    [DisplayFormat(DataFormatString = "n2")]
+    public decimal ManualClientShippedQuantity { set; get; }
+
+    #endregion
 }
